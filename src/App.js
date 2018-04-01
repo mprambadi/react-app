db2550708e42a679086486f4d9782625bf247445
Maul <mprambadi@gmail.com>
2018-04-01 23:46:34 +0700

    add todo-app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,74 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+      items: []
+    };
+  }
+
+  changeInput = e => {
+    this.setState({
+      term: e.target.value
+    });
+  };
+
+  submitInput = e => {
+    e.preventDefault();
+
+    this.setState({
+      term: "",
+      items: [...this.state.items, this.state.term]
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <InputForm
+          submitInput={this.submitInput}
+          term={this.state.term}
+          changeInput={this.changeInput}
+        />
+
+        <List items={this.state.items} />
       </div>
     );
   }
 }
+
+const InputForm = props => {
+  return (
+    <form onSubmit={props.submitInput}>
+      <input
+        type="text"
+        name="to-do"
+        value={props.term}
+        onChange={props.changeInput}
+      />
+      <input type="submit" name="send" value="input" />
+    </form>
+  );
+};
+
+const List = props => {
+  return (
+    <div>
+      {props.items.map((item, index) => {
+        return (
+          <div className="App-underline" key={index}>
+          {/* <p className="App-underline" > */}
+            {item}
+          {/* </p> */}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default App;
