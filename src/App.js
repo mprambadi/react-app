4a724a68f9d8c9e17de12dda22407722f60d2e53
Maul <mprambadi@gmail.com>
2018-04-02 19:37:40 +0700

    to do app ok

--- src/App.js
+++ src/App.js
@@ -3,19 +3,62 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {items:[], text:''}
+    this.handleChange = this.handleChange.bind(this)
+    this.handleInput = this.handleInput.bind(this)
+  }
+ 
+  handleInput(e){
+    e.preventDefault()
+
+    if(!this.state.text){
+      return
+    }
+
+    const newData = {
+      text: this.state.text,
+      id: Date.now()
+    }
+
+    this.setState({
+      items: [...this.state.items, newData],
+      text: ' '
+    })
+  }
+
+  handleChange(e){
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+
   render() {
+    console.log(this.state.items)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <form onSubmit={this.handleInput}>
+          <input type="text" onChange={this.handleChange} value={this.state.text}/>
+          <button> Add #{this.state.items.length + 1}</button>
+        </form>
+
+        <List items={this.state.items}/>
       </div>
     );
   }
 }
+
+const List = (props) => { return (<ul>
+      {props.items.map((item)=>{
+        return <li key={item.id}> {item.text} </li>
+      })}
+    </ul>
+)
+}
+  
+
 
 export default App;
